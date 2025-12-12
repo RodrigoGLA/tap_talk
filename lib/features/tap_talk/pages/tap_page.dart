327a1c3b7198080797a8213254a62f9b5a886aed
Lunari-Roxx <70972565+Lunari-Roxx@users.noreply.github.com>
2025-12-12 02:33:04 -0300

    implements player

--- lib/features/tap_talk/pages/tap_page.dart
+++ lib/features/tap_talk/pages/tap_page.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:tap_talk/features/tap_talk/services/audio_service.dart';
 
-class TapPage extends StatelessWidget{
+class TapPage extends StatefulWidget {
   const TapPage({super.key});
+
+  @override
+  State<TapPage> createState() => _TapPageState();
+}
+
+class _TapPageState extends State<TapPage> {
+  bool _isRecording = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('TapTalk from dart page title'),
-      ),
-      body: const Center(
-        child: Text('TapTalk from dart page body',
-         style: TextStyle(fontSize: 24),),
+      appBar: AppBar(title: const Text('TapTalk from dart page title')),
+      body: Center(
+        child: GestureDetector(
+          onLongPress: () async {
+            setState(() => _isRecording = true);
+            await AudioService.instance.startRecording();
+          },
+          onLongPressUp: () async {
+            await AudioService.instance.stopRecording();
+            setState(() => _isRecording = false,);
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'Grabacion guardada: ${AudioService.instance.lastRecordPath}',
+                ),
+              )
+            );
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            width: 120,
+            height: 120,
+            decoration: BoxDecoration(
+              color: _isRecording ? Colors.red : Colors.blue,
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              _isRecording ? Icons.mic : Icons.mic_none,
+              color: Colors.white,
+              size: 48,
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/features/tap_talk/services/audio_service.dart
+++ lib/features/tap_talk/services/audio_service.dart
@@ -1,8 +1,19 @@
+import 'dart:async';
+
 import 'package:permission_handler/permission_handler.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:record/record.dart';
+import 'package:path_provider/path_provider.dart';
 
 class AudioService {
   static final AudioService instance = AudioService._();
   AudioService._();
+
+  final AudioRecorder _recorder = AudioRecorder();
+  final AudioPlayer _player = AudioPlayer();
+
+  String? _lastRecordPath;
+  String? get lastRecordPath => _lastRecordPath;
 
   Future<bool> ensureMicPermission() async {
     try {
@@ -27,5 +38,78 @@ class AudioService {
       print("Error while checking microphone permission: $e");
       return false;
     }
+  }
+
+  Future<bool> startRecording() async {
+    try {
+      if (!await ensureMicPermission()) {
+        print("Mic permission not garanted.");
+        return false;
+      }
+
+      if (await _recorder.isRecording()) {
+        print('Already recording');
+        return false;
+      }
+
+      const RecordConfig config = RecordConfig(
+        encoder: AudioEncoder.aacLc,
+        bitRate: 128000,
+        sampleRate: 44100,
+      );
+
+      final dir = await getTemporaryDirectory();
+      final filePath =
+          '${dir.path}/my_recording_${DateTime.now().millisecondsSinceEpoch}.m4a';
+
+      await _recorder.start(config, path: filePath);
+      print("Grabando en $filePath");
+      return true;
+    } catch (e) {
+      print("Error al iniciar la grabación: $e");
+      return false;
+    }
+  }
+
+  Future<String?> stopRecording({bool playAfter = true}) async {
+    try {
+      final path = await _recorder.stop();
+      _lastRecordPath = path;
+      print("Archivo guardado en $path");
+
+      if(playAfter && path != null){
+        Timer(const Duration(seconds: 1), () {
+          playLastRecording();
+        });
+      }
+      return path;
+    } catch (e) {
+      print("Error al detener la grabación: $e");
+      return null;
+    }
+  }
+
+    Future<void> playLastRecording() async {
+    if (_lastRecordPath == null) return;
+
+    try {
+      await _player.setFilePath(_lastRecordPath!);
+      _player.play();
+    } catch (e) {
+      print("Error playing audio: $e");
+    }
+  }
+
+  Future<void> stopPlayback() async {
+    try {
+      await _player.stop();
+    } catch (e) {
+      print("Error stopping playback: $e");
+    }
+  }
+
+  Future<void> dispose() async {
+    await _recorder.dispose();
+    await _player.dispose();
   }
 }
